4e51a7ff5a98efd95d72687a66ed19dbff8352da
RawBData <lunarmonky@yahoo.com>
2020-09-29 11:56:44 -0700

    more details

--- models/Posts.js
+++ models/Posts.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-//posts schema that is added to the backend
+//posts schema that is added to the backend that can be acccessed through GraphQL
 const PostSchema = new Schema({
   title: {
     type: String,
