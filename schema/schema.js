ab2defe99cde36b984eb16967d6cb2204d1bf14e
RawBData <lunarmonky@yahoo.com>
2020-10-04 20:24:05 -0700

    add description to schema

--- schema/schema.js
+++ schema/schema.js
@@ -4,6 +4,7 @@ const { GraphQLSchema } = graphql;
 const RootQueryType = require("./root_query_type");
 const mutations = require("./mutations")
 
+//Below is passed in index.js via express.use and rootquerytype is built to create different root queries
 module.exports = new GraphQLSchema({
     // this is the root query object you just created!
     query: RootQueryType,
